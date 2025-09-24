68436533371933f3c4e961c6adb24ee6354baf78
manjaycoder <manjay.verma.coder@gmail.com>
2025-09-24 18:11:49 +0530

    mmam

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         console.log(form);
 
         try {
-            const response = await fetch("http://localhost:3000/api/auth/login", {
+            const response = await fetch("https://chatgpt-1-px0b.onrender.com/api/auth/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
